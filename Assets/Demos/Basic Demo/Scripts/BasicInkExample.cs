71a500755a5f8763ed32477c66dc43fe8093e599
Cristian Rosales <alejandro7120@gmail.com>
2020-09-11 23:34:36 -0400

    add ignore ink package

--- Assets/Demos/Basic Demo/Scripts/BasicInkExample.cs
+++ Assets/Demos/Basic Demo/Scripts/BasicInkExample.cs
@@ -6,7 +6,20 @@ using Ink.Runtime;
 // This is a super bare bones example of how to play and display a ink story in Unity.
 public class BasicInkExample : MonoBehaviour {
     public static event Action<Story> OnCreateStory;
-	
+
+    [SerializeField]
+    private TextAsset inkJSONAsset = null;
+    public Story story;
+
+    [SerializeField]
+    private Canvas canvas = null;
+
+    // UI Prefabs
+    [SerializeField]
+    private Text textPrefab = null;
+    [SerializeField]
+    private Button buttonPrefab = null;
+
     void Awake () {
 		// Remove the default message
 		RemoveChildren();
@@ -95,16 +108,4 @@ public class BasicInkExample : MonoBehaviour {
 		}
 	}
 
-	[SerializeField]
-	private TextAsset inkJSONAsset = null;
-	public Story story;
-
-	[SerializeField]
-	private Canvas canvas = null;
-
-	// UI Prefabs
-	[SerializeField]
-	private Text textPrefab = null;
-	[SerializeField]
-	private Button buttonPrefab = null;
 }
